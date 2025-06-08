2776adcca22c5a9aed16dd40f8105d4ab3feb3b1
Markud <markusgutierrez10@gmail.com>
2025-06-07 22:09:23 -0500

    creacion del fondo letras e imagen

--- flizzpop/lib/feature/home/presentation/view/view_initial.dart
+++ flizzpop/lib/feature/home/presentation/view/view_initial.dart
@@ -23,5 +23,52 @@ class _InitialState extends State<Initial> {
   }
 
   @override
-  
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/fondo.jpeg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AnimatedOpacity(
+                  opacity: _showImage ? 1.0 : 0.0,
+                  duration: Duration(seconds: 5),
+                  child: Image.asset('assets/imgInitial.jpg', width: 500),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "¡Bienvenidos!",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Colors.black.withOpacity(0.3),
+                        offset: Offset(2.0, 2.0),
+                      ),
+                    ],
+                    letterSpacing: 2,
+                    fontFamily: 'Arial',
+                  ),
+                ),
+                SizedBox(height: 20),
+                
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
