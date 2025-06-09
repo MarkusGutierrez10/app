2319f0c136a531f2c1c1ea5e35f443614678ebcc
Markud <markusgutierrez10@gmail.com>
2025-06-08 23:07:47 -0500

    conectando el json con la libreria http

--- flizzpop/lib/feature/home/presentation/bloc/home_bloc.dart
+++ flizzpop/lib/feature/home/presentation/bloc/home_bloc.dart
@@ -13,7 +13,16 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
     on<HomeSearchPressed>((event, emit) async{
       emit(HomeLoadInProgress());
-      
+      final url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
+      final response = await http.get(url);
+      if (response.statusCode == 200){
+        Map objectMap = jsonDecode(response.body);
+        User user = User(objectMap);
+        emit(HomeLoadSuccess(user));
+      }else{
+        emit(HomeLoadFailure());
+      }
+
     });
   }
 }
