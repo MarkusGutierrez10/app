7246f26de4acfaa646be8f31e480b5bc8fbc90e3
Markud <markusgutierrez10@gmail.com>
2025-06-08 23:07:00 -0500

    usando bloc para cambios de estados

---
+++ flizzpop/lib/feature/home/presentation/bloc/home_bloc.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../data/models/user.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeInitial()) {
+    on<HomeSearchPressed>((event, emit) async{
+      emit(HomeLoadInProgress());
+      
+    });
+  }
+}
