286ba692715058fb1b1df123355735c483b0bc23
Markud <markusgutierrez10@gmail.com>
2025-06-09 05:43:59 -0500

    lo que pude adelantar

--- flizzpop/lib/main.dart
+++ flizzpop/lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:flizzpop/feature/home/presentation/bloc/home_bloc.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'feature/data/models/producto.dart';
+import 'feature/home/presentation/bloc/home_bloc.dart';
 import 'feature/home/presentation/view/view_Sucess.dart';
 import 'feature/home/presentation/view/view_failure.dart';
 import 'feature/home/presentation/view/view_loading.dart';
